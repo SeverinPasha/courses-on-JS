ebf17796e98e6a828b6645c27eb755a6fe61933f
Severin Pasha <shadowthet@gmail.com>
2017-11-10 23:53:12 +0300

    first commit

---
+++ list/index.js
@@ -0,0 +1,12 @@
+function add() {
+    var a = document.getElementById("input").value;
+    var b = document.getElementById("items");
+    var c = document.createElement("li");
+    c.innerHTML = a;
+    b.append(c);
+    document.getElementById("input").value = "";
+
+//нифига еще не сделал
+//всё доделаю   
+//но не сразу
+}
