6a6f3adfc5d6a43ddfdbd52d09862d70b69581a9
Severin Pasha <shadowthet@gmail.com>
2017-11-12 20:41:29 +0300

    test commit

--- list/index.js
+++ list/index.js
@@ -1,26 +1,32 @@
+function deleteEl(el) {
+    el.parentElement.remove();
+}
+
 function todoList() {
     var item = document.getElementById('todoInput').value;
     var text = document.createTextNode(item);
     var checkbox = document.createElement('input');
     var button = document.createElement('button');
 
+    button.innerHTML = "Delete";
+    button.setAttribute("onclick", "deleteEl(this)");
+
     checkbox.type = "checkbox";
     checkbox.name = "name";
     checkbox.value = "value";
     var newItem = document.createElement("div");
-    
-   
 
-    if(item !== "" ){
+
+
+    if (item !== "") {
         newItem.appendChild(checkbox);
         newItem.appendChild(text);
         newItem.appendChild(button);
-        
+
         document.getElementById("todoList").appendChild(newItem)
         document.getElementById("todoInput").value = "";
-    }
-    else {
-        alert ("Заполните поле");
+    } else {
+        alert("Заполните поле");
     }
 
 
